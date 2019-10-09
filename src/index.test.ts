db04f711b3b8e93ac6b9d7c2f016b27567fd4a9b
mohoff <mohoff@web.de>
2019-10-09 18:08:53 +0200

    refactor: export a class instead of two functions
    BREAKING CHANGE: The exported class StrongConfig must be instantiated to
    access load() and validate(). Both functions are not top-level exports
    anymore

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,57 @@
-import * as strongConfig from './index'
+jest.mock('./load')
+jest.mock('./validate')
 
-describe('strong-config exports are as expected', () => {
-  it('exports a load function', () => {
-    expect(strongConfig.load).toBeInstanceOf(Function)
+import { load } from './load'
+import { validate } from './validate'
+
+const mockedLoad = load as jest.MockedFunction<typeof load>
+const mockedValidate = validate as jest.MockedFunction<typeof validate>
+
+const mockedConfig = { some: 'config', runtimeEnvironment: 'development' }
+const mockedValidationResult = true
+mockedLoad.mockReturnValue(mockedConfig)
+mockedValidate.mockReturnValue(mockedValidationResult)
+
+import StrongConfig from './index'
+
+describe('StrongConfig class', () => {
+  it('StrongConfig is a class that can be instantiated', () => {
+    expect(new StrongConfig()).toBeDefined()
   })
 
-  it('exports a validate function', () => {
-    expect(strongConfig.validate).toBeInstanceOf(Function)
+  it('StrongConfig accepts and stores a configuration object', () => {
+    const mockConfiguration = {}
+    const strongConfig = new StrongConfig(mockConfiguration)
+
+    expect(strongConfig.configuration).toStrictEqual(mockConfiguration)
+  })
+
+  it('strongConfig exposes "load()"', () => {
+    expect(new StrongConfig().load).toBeInstanceOf(Function)
+  })
+
+  it('strongConfig exposes "validate()"', () => {
+    expect(new StrongConfig().validate).toBeInstanceOf(Function)
+  })
+
+  it('strongConfig.load() calls imported load() and returns its result', () => {
+    const strongConfig = new StrongConfig()
+
+    const result = strongConfig.load()
+
+    expect(mockedLoad).toHaveBeenCalledTimes(1)
+    expect(result).toStrictEqual(mockedConfig)
+  })
+
+  it('strongConfig.validate() calls imported validate() and returns its result', () => {
+    const strongConfig = new StrongConfig()
+
+    const result = strongConfig.validate(
+      'path/to/schema.json',
+      'some/config/path'
+    )
+
+    expect(mockedValidate).toHaveBeenCalledTimes(1)
+    expect(result).toStrictEqual(mockedValidationResult)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
-export { load } from './load'
-export { validate } from './validate'
+import { load } from './load'
+import { validate } from './validate'
+
+interface Configuration {
+  // TODO: define Configuration interface
+}
+
+export default class StrongConfig {
+  public readonly configuration: Configuration | undefined
+
+  constructor(configuration?: Configuration) {
+    // TODO: validate configuration
+    this.configuration = configuration
+  }
+
+  public load(passedConfig?: MemoizedConfig): ReturnType<typeof load> {
+    return load(passedConfig)
+  }
+
+  public validate(
+    schemaPath: string,
+    ...configPaths: string[]
+  ): ReturnType<typeof validate> {
+    return validate(schemaPath, ...configPaths)
+  }
+}
