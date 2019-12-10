7049f5b6b578359cecc38f7fa40993beb3f1c41b
mohoff <mohoff@web.de>
2019-12-10 17:52:02 +0100

    fix: --verbose flag properly working for decrypt/encrypt CLI commands

--- src/cli/commands/validate.test.ts
+++ src/cli/commands/validate.test.ts
@@ -1,16 +1,39 @@
 jest.mock('../../validate')
+jest.mock('../spinner')
 
 import stdMocks from 'std-mocks'
 
 import { validate as validateUtil } from '../../validate'
+import {
+  startSpinner,
+  failSpinner,
+  succeedSpinner,
+  getVerbosityLevel,
+  VerbosityLevel,
+} from '../spinner'
+
 const mockedValidateUtil = validateUtil as jest.MockedFunction<
   typeof validateUtil
 >
+const mockedStartSpinner = startSpinner as jest.MockedFunction<
+  typeof startSpinner
+>
+const mockedFailSpinner = failSpinner as jest.MockedFunction<typeof failSpinner>
+const mockedSuceedSpinner = succeedSpinner as jest.MockedFunction<
+  typeof succeedSpinner
+>
+const mockedGetVerbosityLevel = getVerbosityLevel as jest.MockedFunction<
+  typeof getVerbosityLevel
+>
+
+mockedGetVerbosityLevel.mockReturnValue(VerbosityLevel.Verbose)
 
 import Validate from './validate'
 
 const configPath = 'example/development.yaml'
 const schemaPath = 'example/schema.json'
+
+const validationError = new Error('some validation error')
 
 const mockedExit = jest.spyOn(process, 'exit').mockImplementation()
 
@@ -56,17 +79,8 @@ describe('strong-config validate', () => {
   })
 
   describe('handles validation', () => {
-    beforeAll(() => {
-      stdMocks.use()
-    })
-
     beforeEach(() => {
       jest.clearAllMocks()
-      stdMocks.flush()
-    })
-
-    afterEach(() => {
-      stdMocks.restore()
     })
 
     it('exits with code 0 when successful', async () => {
@@ -75,13 +89,52 @@ describe('strong-config validate', () => {
       expect(mockedExit).toHaveBeenCalledWith(0)
     })
 
+    it('exits with code 1 when validation fails', async () => {
+      mockedValidateUtil.mockImplementationOnce(() => {
+        throw validationError
+      })
+
+      await Validate.run([configPath, schemaPath])
+
+      expect(mockedExit).toHaveBeenCalledWith(1)
+    })
+
     it('validates against schema', async () => {
       await Validate.run([configPath, schemaPath])
 
       expect(mockedValidateUtil).toHaveBeenCalledTimes(1)
     })
+  })
 
-    // TODO: Investigate why this isn't writing to mocked stderr as it does with the decryption output
-    test.todo('shows the validation status')
+  describe('informs the user', () => {
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it('informs user about the validation process', async () => {
+      await Validate.run([configPath, schemaPath])
+
+      expect(mockedStartSpinner).toHaveBeenCalledWith('Validating...')
+    })
+
+    it('informs user about the validation result', async () => {
+      await Validate.run([configPath, schemaPath])
+
+      expect(mockedSuceedSpinner).toHaveBeenCalledWith('Validated!')
+    })
+
+    it('informs user about validation errors', async () => {
+      mockedValidateUtil.mockImplementationOnce(() => {
+        throw validationError
+      })
+
+      await Validate.run([configPath, schemaPath])
+
+      expect(mockedFailSpinner).toHaveBeenCalledWith(
+        'Config validation against schema failed',
+        expect.any(Error),
+        VerbosityLevel.Verbose
+      )
+    })
   })
 })
