ca27d16a158f9f4e9645f7a9f4d2f282a66997de
Philip Paetz <philip@brickblock.io>
2020-02-14 16:57:49 +0100

    feat(DX): improved error message when process.env.NODE_ENV is undefined

--- src/index.ts
+++ src/index.ts
@@ -24,25 +24,35 @@ export = class StrongConfig {
     this.runtimeEnv = process.env[this.options.runtimeEnvName]
   }
 
-  public load(): ReturnType<typeof load> {
+  private checkRuntimeEnv(): void {
     if (R.isNil(this.runtimeEnv)) {
-      throw new Error('runtimeEnv must be defined.')
+      throw new Error(
+        `[💪 strong-config] Can't load config: process.env.${this.options.runtimeEnvName} is undefined.
+         Maybe you have forgotten to set the environment variable ${this.options.runtimeEnvName} when starting your application?
+         For example: 'NODE_ENV=production yarn start'\n`
+      )
     }
+  }
+
+  public load(): ReturnType<typeof load> {
+    this.checkRuntimeEnv()
 
     if (this.config) {
       return this.config
     }
 
+    /* eslint-disable-next-line */
+    // @ts-ignore because we're checking for 'undefined' in this.checkRuntimeEnv() already
     this.config = load(this.runtimeEnv, this.options)
 
     return this.config
   }
 
   public validate(): ReturnType<typeof validate> {
-    if (R.isNil(this.runtimeEnv)) {
-      throw new Error('runtimeEnv must be defined.')
-    }
+    this.checkRuntimeEnv()
 
+    /* eslint-disable-next-line */
+    // @ts-ignore because we're checking for 'undefined' in this.checkRuntimeEnv() already
     return validate(this.runtimeEnv, this.options.configRoot)
   }
 }
