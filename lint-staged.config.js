f2824ddbfe611e99642be90699181097ce509e11
Philip Paetz <philip@brickblock.io>
2020-05-19 16:17:36 +0200

    chore: auto-validate example config files before every commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,10 @@ module.exports = {
   '*.ts': ['eslint --fix', 'git add'],
   '*.json': ['jsonlint-cli', 'git add'],
   '*.{yaml,yml}': ['yamllint', 'git add'],
+  'example/*.{yaml,yml}': [
+    'strong-config validate --config-root example',
+    'git add',
+  ],
   '*.md': [
     'markdownlint --ignore CHANGELOG.md --ignore node_modules',
     'git add',
