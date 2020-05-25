ea5780c774ad08bb5574c443999ec5d089da8ea6
Philip Paetz <philip@brickblock.io>
2020-05-25 18:15:22 +0200

    feat(sops): improved error handling when sops gets a permission-denied error

--- src/utils/sops.ts
+++ src/utils/sops.ts
@@ -8,50 +8,34 @@ const hasSopsMetadata = has('sops')
 
 export const runSopsWithOptions = (options: string[]): string => {
   let sopsResult
-  try {
-    // Trying to use global `sops` from $PATH first
-    console.log('try global sops')
 
+  try {
     sopsResult = sync('sops', options)
   } catch (error) {
-    console.log('❌ No global sops found', error.code)
-
-    // If the error code is ENOENT, it likely means the 'sops' binary
-    // isn't available in the global path. In this case, we want to swallow
-    // the error here and retry with a local sops binary further down
-    if (error.code !== 'ENOENT') {
-      console.log('unexpected error with global sops', error.code)
-
-      throw new Error(error)
-    }
-  }
-
-  if (!sopsResult) {
-    /*
-     * If `sops` binary could not be found in $PATH, search in current directory
-     * This is useful for serverless environments such as Google Cloud Functions
-     * where it's not possible to install any non-npm binaries in the environment
-     */
-    try {
-      console.log('try local sops')
-      sopsResult = sync('./sops', options)
-    } catch (error) {
-      console.log('❌No local sops found', error.code)
-
+    if (!sopsResult) {
+      // If the error code is ENOENT, it likely means the 'sops' binary
+      // isn't available in the global $PATH. In this case, try again
+      // with a local 'sops' binary from the current directory.
+      // This is useful for serverless environments such as Google Cloud Functions
+      // where it's not possible to install any non-npm binaries in the environment.
       if (error.code === 'ENOENT') {
-        throw new Error(
-          `❌ Couldn't find 'sops' binary neither in $PATH nor in current directory via 'process.cwd()'\nPlease make sure it is available in your runtime environment.\n\n${error}`
-        )
+        sopsResult = sync('./sops', options)
       } else {
-        console.log('unexpected error with local sops', error.code)
-
+        // If local sops also fails, bubble up the error
         throw new Error(error)
       }
     }
   }
 
-  if (sopsResult.exitCode !== 0) {
-    throw new Error(sopsResult.toString())
+  switch (sopsResult.exitCode) {
+    case 0:
+      break
+    case 128:
+      throw new Error(
+        `Sops failed to retrieve the decryption key. This is often a permissin error with your KMS provider.\n${sopsResult}`
+      )
+    default:
+      throw new Error(sopsResult.toString())
   }
 
   return sopsResult.stdout.toString()
