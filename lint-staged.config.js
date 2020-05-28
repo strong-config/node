b9aad88656a1dba0f08942049dba24da38e7aec7
Philip Paetz <philip@brickblock.io>
2020-05-28 14:50:51 +0200

    chore: update lint-staged config. no need to explicitly call anymore as it's not run on default for every lint-staged task

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-  '*.ts': ['eslint --fix', 'git add'],
-  '*.json': ['jsonlint-cli', 'git add'],
-  '*.{yaml,yml}': ['yamllint', 'git add'],
-  'example/*.{yaml,yml}': [
-    'strong-config validate --config-root example',
-    'git add',
-  ],
-  '*.md': [
-    'markdownlint --ignore CHANGELOG.md --ignore node_modules',
-    'git add',
-  ],
+  '*.ts': ['eslint --fix'],
+  '*.json': ['jsonlint-cli'],
+  '*.{yaml,yml}': ['yamllint'],
+  'example/*.{yaml,yml}': ['strong-config validate --config-root example'],
+  '*.md': ['markdownlint --ignore CHANGELOG.md --ignore node_modules'],
 }
