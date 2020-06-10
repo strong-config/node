5616b83badd4f71c9a0fbe8bbae4cab9226033cb
Philip Paetz <philip@brickblock.io>
2020-06-10 21:20:11 +0200

    chore: extracted pascalCase() util into its own file

---
+++ src/utils/pascal-case.test.ts
@@ -0,0 +1,13 @@
+import { pascalCase } from './pascal-case'
+
+describe('pascalCase()', () => {
+  test.each([
+    ['name', 'Name'],
+    ['some-name', 'SomeName'],
+    ['some name', 'SomeName'],
+    ['some name----asdf', 'SomeNameAsdf'],
+    ['some name----asdf !@#($@^!)#% camelCase', 'SomeNameAsdfCamelCase'],
+  ])('given %s, it correctly pascalCases to %s', (input, expectedOutput) => {
+    expect(pascalCase(input)).toBe(expectedOutput)
+  })
+})
---
+++ src/utils/pascal-case.ts
@@ -0,0 +1,10 @@
+/*
+ * json-schema-to-typescript uses a `toSafeString(string)` function to obtain a normalized string.
+ * This pascalCase mimics this functionality and should address most cases.
+ * => https://github.com/bcherny/json-schema-to-typescript/blob/f41945f19b68918e9c13885f345cb708e1d9898a/src/utils.ts#L163)
+ */
+export const pascalCase = (input: string): string =>
+  input
+    .split(/[^\dA-Za-z]+/g)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join('')
